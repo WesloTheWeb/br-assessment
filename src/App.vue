72257770d52ec5e32879851edb7df1f4b15a51d6
Wesley <wesleyadwebster@gmail.com>
2023-09-17 21:05:58 -0500

    Deleted unused files and set up styling and main component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1>BR Intake Form</h1>
+  <UserForm />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import UserForm from './components/Form/UserForm.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    UserForm
   }
 });
 </script>
-
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './styles/main.scss';
 </style>
---
+++ src/components/Form/UserForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <form>
+
+    </form>
+</template>
+  
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'UserForm',
+});
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import './UserForm.scss';
+</style>
